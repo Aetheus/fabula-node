44aab985c175eb3c5b3331d940fcdd8e823ccdd7
adrian <adrianhyk@gmail.com>
2015-03-23 09:49:28 +0800

    Revert to 55431171e5503eaee1167adc7e725a98538cb719

--- routes/login.js
+++ routes/login.js
@@ -1,22 +1,54 @@
 var express = require("express");
 var router = express.Router();
+var verifylogin = require("../utility/verifylogin");
+
 
 router.get("/", function (req,res,next){
-	res.render("login", { message:"Enter your details to login" });
-});
-
-router.post("/", function (req,res,next){
+	var returnMsg = "Enter your details to login";
 	var sess = req.session;
 
-	if(req.body.username){
-		sess.username = req.body.username;
-		console.log("User signed in as " + sess.username);
-
-		var returnMsg = "You're already logged in, " + sess.username;
-		res.render("login", { message:returnMsg })
+	if(sess.username){
+		returnMsg = "You're already logged in, " + sess.username;
 	}else{
-		res.location("./login");
+		returnMsg = "Enter your details to login";
 	}
+
+	res.render("login", { message:returnMsg });
+});
+
+/*router.get("/makeerr", function (req,res,next){
+	next(new Error("test for error handler. if you're seeing this, the error handler worked"));
+});*/
+
+router.post("/", function (req,res,next){
+	console.log("made it to post");
+	var sess = req.session;
+	var returnMsg = "Default return message";
+
+	if(req.body.username && req.body.password){
+		verifylogin(req.body.username, req.body.password, function (err, isVerified, user){
+			if (err){
+				//this calls our error handler
+				next(err);	
+			} 
+
+			
+			if (isVerified){
+				sess.username = req.body.username;
+				returnMsg = "You're already logged in, " + sess.username;
+				console.log("User signed in as " + sess.username);
+			}else{
+				returnMsg = "Incorrect username or password";
+			}
+
+			res.render("login", {message:returnMsg});
+		});
+
+	}else{
+		returnMsg="Enter username and password to proceed";
+		res.render("login", { message:returnMsg });
+	}
+
 })
 
 module.exports = router;
