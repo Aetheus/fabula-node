a18cb9121a48d851cc7b80c49cab13fd8d9eb92d
adrian <adrianhyk@gmail.com>
2015-03-26 16:30:17 +0800

    change allow-origin header to req.headers.origin

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(req, res, next){
 //CORS to enable cross domain requests
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
