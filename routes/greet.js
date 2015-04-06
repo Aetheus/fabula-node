12d21ca46e0fbf8d2e4b49125127b83abe3b0ad5
Adrian Heng <adrianhyk@gmail.com>
2015-04-07 02:04:20 +0800

    add post version

--- routes/greet.js
+++ routes/greet.js
@@ -30,6 +30,34 @@ router.get("/", function (req, res, next){
 
 });
 
+//POST version of the prevoius one
+router.post("/", function (req, res, next){
+	
+	var returnObj = {
+		isMember: false,
+		name: "guest"
+	}
+
+	if (!(req.body.username && req.body.password)){
+		returnObj.name = "careless guest";
+		res.json(returnObj);
+	}else{
+		verifylogin(req.body.username, req.body.password, function (err, isVerified, userRow){
+			if (err) return next(err);
+
+			if(isVerified){
+				returnObj.isMember = true;
+				returnObj.name = req.body.username;
+			}else{
+				returnObj.name = "unauthorized guest";
+			}
+
+			res.json(returnObj);
+		});
+	}
+
+});
+
 
 
 module.exports = router;
