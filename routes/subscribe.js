a16ac6e9402b372ea9c195f4b06bfb681f9b6433
Adrian Heng <adrianhyk@gmail.com>
2015-05-31 03:48:47 +0800

    Added more indepth documentation to the subscribe.js route

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -1,34 +1,39 @@
+//var express = require("express") tells our program to include the express module into this script.
+//likewise, require("../utility/config") tells our program to include the config.js file into this script
+//similar in concept to the "import" keyword in Java and the "include" keyword in C++
 var express = require("express");
 var router = express.Router();
 var pg = require("pg");
 var config = require("../utility/config");
 var verifylogin = require("../utility/verifylogin");
 var reservedwords = require("../utility/reservedwords");
-
 var feedchannel = (require("../model/FeedChannel"))();
 
 
-router.post("/", function (req, res, next){
+//this creates a "route". When a POST request is sent to "https://fabula-node.herokuapp.com/subscribe.js", 
+//it will be bound to this route and execute the callback function shown below.
+router.post("/", function (req, res, next){	// << this is the callback function 
+
+	//req.body is an object that holds the value of parameters that were sent with the POST request
+	//therefore, the variables below this are being assigned the value of  parameters sent with the POST request
+	//for instance, req.body.channelname is retrieving the "channelname" parameter from the post request	
 	var channelname = (req.body.channelname != undefined) 		? req.body.channelname : null;
 	var channelIsCustom = (req.body.iscustom != undefined) 		? req.body.iscustom : true;
 
 	var titleSelector = (req.body.title != undefined) 			? req.body.title : reservedwords.dbNULL;
 	var linkSelector = (req.body.link != undefined) 			? req.body.link : reservedwords.dbNULL;
 	var descriptionSelector = (req.body.description != undefined) ? req.body.description : reservedwords.dbNULL;
-	var imageLinkSelector = (req.body.imagelink != undefined) 	? req.body.imagelink : reservedwords.dbNULL;
-	
-
+	var imageLinkSelector = (req.body.imagelink != undefined) 	? req.body.imagelink : reservedwords.dbNULL;	
 	var tags = (req.body.tags != undefined) 	? req.body.tags : reservedwords.dbNULL;
 
 	var ancestorSelector = (req.body.ancestor != undefined) 	? req.body.ancestor : null;
 	var siteURL = (req.body.site != undefined) 					? req.body.site : null;
 	
-
-	var session = req.session;
 	var userid = (req.body.username != undefined) ? req.body.username : null;
 	var password= (req.body.password != undefined) ? req.body.password : null;
 
 
+	//if the user ID was NOT sent with the POST request, return an error.
 	if (!userid){
 		return next(new Error("You must be signed in to subscribe to a feed!"));
 	}
@@ -37,11 +42,16 @@ router.post("/", function (req, res, next){
 	}
 
 
-	verifylogin(userid,password, function (err, isVerified, user){
-		if (err) return next(err);
+	//verify the user's login, and then execute the callback function below
+	verifylogin(userid,password, function (err, isVerified, user){	//<< callback function, to be executed only after verification
+		//if an error occured, return the error
+		if (err) return next(err);	
 
+		//if the user could not be verified, return a custom error informing them that they entered invalid details
 		if (!isVerified) return next(new Error("Invalid login details provided!"));
 	
+		//create a key-value pair object that holds values to be inserted into the tblFeedChannel table in our database
+		//this key-value pair object holds the values of the parameters we extracted from the POST request.
 		var insertDictionary = {
 			"fedUserID":userid,
 			"fedFeedChannelName":channelname,
@@ -57,29 +67,29 @@ router.post("/", function (req, res, next){
 			"fedFeedChannelTags"	 : tags
 		}
 
+		//this is for our upsert function - if there already exists a feed channel for this specific user and URL,
+		//we will UPDATE that feed channel. Otherwise, we will INSERT a new one.
 		var whereDictionary = {
 			"fedUserID":userid,
 			"fedFeedChannelURL":siteURL,
 		}
 
 
-		
+		//we've written a handy "upsert" function for our tables. This allows us to INSERT a row ONLY IF a row matching the columns
+		//of our whereDictionary object doesn't exist in our database. If such a row exists, this function performs an UPDATE instead.
 		feedchannel.upsert(insertDictionary, whereDictionary, function (err, result){
 			if (err){
-				if (err.code == 23505){
-					return next(new Error("Duplicate key error!"));		
-				}else{
-					return next(err);		
-				}
+				return next(err);		
+				/*if (err.code == 23505){return next(new Error("Duplicate key error!"));}else{}*/
 			} 
-	
+		
+			//set the response header to JSON and return the results of this subscription back to the requester
 			res.setHeader('Content-Type', 'application/json');
 			res.end(JSON.stringify(result));
 		})
 
 	})
-
-  	
+	
 });
 
 /*
